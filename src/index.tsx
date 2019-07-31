076173a3cdf718f07989bf2295b4c84cf843fbd1
Sudhanshu Tripathi <sudhanshu.tripathi@1rivet.local>
2019-07-31 12:32:39 +0530

    misc update

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
+import 'core-js/stable';
 import "react-app-polyfill/ie9"
 import "react-app-polyfill/ie11"
-import "react-app-polyfill/stable"
-import 'raf/polyfill';
-import 'core-js/es/map';
-import 'core-js/es/set';
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import "react-datepicker/dist/react-datepicker.css";
